b9b81faed760958817b9c61bfae679f5778b230b
VladyslavSak <vladyslav.sak@gmail.com>
2019-10-01 18:20:43 +0300

    Updates

---
+++ precache-manifest.8ed8617b031667585f4f05492014be31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42120c4eefe452e6a0c15a4a7e0a061e",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "54dba4c95339024e3d66",
+    "url": "/react_tabs-with-router/static/css/main.3fc72d5d.chunk.css"
+  },
+  {
+    "revision": "7f4084f4cc66b9079733",
+    "url": "/react_tabs-with-router/static/js/2.9442a586.chunk.js"
+  },
+  {
+    "revision": "54dba4c95339024e3d66",
+    "url": "/react_tabs-with-router/static/js/main.1215f432.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
